4332c307f7066dfb1283ac4343bc1a2ed95554f8
Jason Xie <xzp1990@gmail.com>
2020-05-25 11:53:58 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Telstra CI/CD</h1>
+                <h1>Jaosn's CI/CD workshop</h1>
                 <h2>Congratulations</h2>
                 <p>Your Telstra application is now running on a Docker container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
